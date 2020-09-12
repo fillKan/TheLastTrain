928c75a4564edb569a173a693a1611f86dc9821b
한대건 <han15050424@gmail.com>
2020-09-12 13:27:39 +0900

    광원의 변화를 지정하는 LightSource스크립트를 추가한다
    LightSource스크립트는 LightInfo구조체에 기반하여 광원의 변화를 설정한다.
    LightSource스크립트는 '정오에 가까워 질수록'의 변화와 '자정에 가까워 질수록'의 변화를
    지정할 수 있게 되어있다. 정오에 가까워진다는 말은 광원이 화면 정중앙으로 향할때를 말하고,
    자정에 가까워진다는 말은 화면 정중앙에 반대편으로 향할때를 말한다.
    
    지금은 이런 계획만 있을 뿐, 아무런 행동도 없는 상태이다..

---
+++ Assets/LightSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[System.Serializable]
+public struct LightInfo
+{
+    public Color color;
+    public float scale;
+}
+
+public class LightSource : MonoBehaviour
+{
+    [SerializeField] private Light2D mLight2D;
+
+    [SerializeField] private LightInfo LightOfMidDay;
+    [SerializeField] private LightInfo LightOfMidNight;
+
+    private void Reset()
+    {
+        Debug.Assert(TryGetComponent(out mLight2D));
+    }
+}
