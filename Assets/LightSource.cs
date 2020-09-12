9fb3ccb92177baa15ff9f3768027cd78232c093d
한대건 <han15050424@gmail.com>
2020-09-12 17:31:39 +0900

    LightSource스크립트 내의 중복된 코드를 제거한다
    지역변수를 사용하여 값을 설정하고, 그 값을 바탕으로 색상을 변화하도록 한다.

--- Assets/LightSource.cs
+++ Assets/LightSource.cs
@@ -22,41 +22,65 @@ public class LightSource : MonoBehaviour
     }
     private void Update()
     {
+        float lerpAmount = 0f;
+
+        Color targetColor = Color.white;
+        Color tracerColor = Color.white;
+
         if (mIsSun)
         {
+            // Dawn -> Noon
             if (mRevolution.Angle <= 90)
             {
-                // Dawn -> Noon
-                mLight2D.color = Color.Lerp(ColorOfNoon, ColorOfDawn, mRevolution.Angle / 90f);
+                targetColor = ColorOfNoon;
+                tracerColor = ColorOfDawn;
+
+                lerpAmount = mRevolution.Angle / 90f;                
             }
+            // Noon -> Duty
             else if (mRevolution.Angle <= 360 && mRevolution.Angle > 270)
             {
-                // Noon -> Duty
-                mLight2D.color = Color.Lerp(ColorOfDuty, ColorOfNoon, (mRevolution.Angle - 270f) / 90f);
+                targetColor = ColorOfDuty;
+                tracerColor = ColorOfNoon;
+
+                lerpAmount = (mRevolution.Angle - 270f) / 90f;
             }
+            // Duty -> Dawn
             else if (mRevolution.Angle <= 270 && mRevolution.Angle > 180)
             {
-                // Duty -> Dawn
-                mLight2D.color = Color.Lerp(ColorOfDawn, ColorOfDuty, (mRevolution.Angle - 180f) / 90f);
+                targetColor = ColorOfDawn;
+                tracerColor = ColorOfDuty;
+
+                lerpAmount = (mRevolution.Angle - 180f) / 90f;
             }
         }
         else
-        {  
+        {
+            // Dawn -> Noon
             if (mRevolution.Angle <= 270 && mRevolution.Angle > 180)
             {
-                // Dawn -> Noon
-                mLight2D.color = Color.Lerp(ColorOfNoon, ColorOfDawn, (mRevolution.Angle - 180f) / 90f);
+                targetColor = ColorOfNoon;
+                tracerColor = ColorOfDawn;
+
+                lerpAmount = (mRevolution.Angle - 180f) / 90f;
             }
+            // Noon -> Duty
             else if (mRevolution.Angle <= 180 && mRevolution.Angle > 90)
             {
-                // Noon -> Duty
-                mLight2D.color = Color.Lerp(ColorOfDuty, ColorOfNoon, (mRevolution.Angle - 90f) / 90f);
+                targetColor = ColorOfDuty;
+                tracerColor = ColorOfNoon;
+
+                lerpAmount = (mRevolution.Angle - 90f) / 90f;
             }
+            // Duty -> Dawn
             else if (mRevolution.Angle <= 90)
             {
-                // Duty -> Dawn
-                mLight2D.color = Color.Lerp(ColorOfDawn, ColorOfDuty, mRevolution.Angle / 90f);
+                targetColor = ColorOfDawn;
+                tracerColor = ColorOfDuty;
+
+                lerpAmount = mRevolution.Angle / 90f;
             }
         }
+        mLight2D.color = Color.Lerp(targetColor, tracerColor, lerpAmount);
     }
 }
