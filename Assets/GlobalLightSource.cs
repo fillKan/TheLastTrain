15bd213a3aed898779b2c2c5d6d172a378489254
한대건 <han15050424@gmail.com>
2020-09-12 17:17:22 +0900

    GlobalLightSource스크립트를 구현하고, 이를 씬에 적용한다
    각 시간별로 색상의 변화가 일어나도록 구현하고, 이를 씬에있는 Global Light오브젝트에 적용 시킨다.
    변화하는 색상은 임의로 지정했다 좀 예쁜듯ㅎㅎ

--- Assets/GlobalLightSource.cs
+++ Assets/GlobalLightSource.cs
@@ -5,10 +5,63 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class GlobalLightSource : MonoBehaviour
 {
+    [SerializeField] private Revolution mRevolution;
+
     [SerializeField] private Light2D mLight2D;
 
     [SerializeField] private Color ColorOfDawn;
     [SerializeField] private Color ColorOfNoon;
     [SerializeField] private Color ColorOfDuty;
     [SerializeField] private Color ColorOfMidNight;
+
+    private IEnumerator mEUpdate;
+
+    private void Reset()
+    {
+        ColorOfDawn = Color.white;
+        ColorOfNoon = Color.white;
+        ColorOfDuty = Color.white;
+
+        ColorOfMidNight = Color.white;
+
+        Debug.Assert(TryGetComponent(out mLight2D));
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(mEUpdate = EUpdate());
+    }
+    private void OnDisable()
+    {
+        if (mEUpdate != null) {
+            StopCoroutine(mEUpdate);
+        }
+        mEUpdate = null;
+    }
+    private IEnumerator EUpdate()
+    {
+        while (gameObject.activeSelf)
+        {
+            if (mRevolution.Angle <= 90f)
+            {
+                // Dawn -> Noon
+                mLight2D.color = Color.Lerp(ColorOfNoon, ColorOfDawn, mRevolution.Angle / 90f);
+            }
+            else if (mRevolution.Angle <= 360 && mRevolution.Angle > 270)
+            {
+                // Noon -> Duty
+                mLight2D.color = Color.Lerp(ColorOfDuty, ColorOfNoon, (mRevolution.Angle - 270f) / 90f);
+            }
+            else if (mRevolution.Angle <= 270 && mRevolution.Angle > 180)
+            {
+                // Duty -> MidNight
+                mLight2D.color = Color.Lerp(ColorOfMidNight, ColorOfDuty, (mRevolution.Angle - 180f) / 90f);
+            }
+            else if (mRevolution.Angle <= 180 && mRevolution.Angle > 90)
+            {
+                // MidNight -> Dawn
+                mLight2D.color = Color.Lerp(ColorOfDawn, ColorOfMidNight, (mRevolution.Angle - 90f) / 90f);
+            }
+            yield return null;
+        }
+    }
 }
