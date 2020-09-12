a2c775af6cd071379946734fc2abf3d012d0167a
한대건 <han15050424@gmail.com>
2020-09-12 16:57:14 +0900

    더이상 사용하지 않는 구조체 LightInfo를 제거한다

--- Assets/LightSource.cs
+++ Assets/LightSource.cs
@@ -4,13 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-[System.Serializable]
-public struct LightInfo
-{
-    public Color color;
-    public float scale;
-}
-
 public class LightSource : MonoBehaviour
 {
     [SerializeField] private bool mIsSun;
