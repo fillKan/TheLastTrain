3cc37b271fa44d289f5a86f49412df05ed11b891
한대건 <han15050424@gmail.com>
2020-09-12 14:57:05 +0900

    공전 위치에 따른 광원의 변화를 구현한다
    현재 광원이 있는 위치에따라(Revolution의 회전각 값을 기준으로 판단함) 인스펙터로 설정한
    LightInfo의 정보대로 광원에 변화를 준다.
    
    IsSun의 값이 참이면 태양이고, 그렇지 않다면 달로 구분한다. 이러한 구분은 '달이 저물고 있을때 해가 떠오른다'
    오 같은 행동을 구분하기 위함이다.

--- Assets/LightSource.cs
+++ Assets/LightSource.cs
@@ -13,6 +13,10 @@ public struct LightInfo
 
 public class LightSource : MonoBehaviour
 {
+    [SerializeField] private bool mIsSun;
+
+    [SerializeField] private Revolution mRevolution;
+
     [SerializeField] private Light2D mLight2D;
 
     [SerializeField] private LightInfo LightOfMidDay;
@@ -21,5 +25,38 @@ public class LightSource : MonoBehaviour
     private void Reset()
     {
         Debug.Assert(TryGetComponent(out mLight2D));
+    }
+    private void Update()
+    {
+        if (mIsSun)
+        {
+            if (mRevolution.Angle <= 180)
+            {
+                mLight2D.pointLightOuterRadius = Mathf.Lerp(LightOfMidDay.scale, LightOfMidNight.scale, mRevolution.Angle / 180);
+
+                mLight2D.color = Color.Lerp(LightOfMidDay.color, LightOfMidNight.color, mRevolution.Angle / 180);
+            }
+            else if (mRevolution.Angle <= 360)
+            {
+                mLight2D.pointLightOuterRadius = Mathf.Lerp(LightOfMidNight.scale, LightOfMidDay.scale, (mRevolution.Angle - 180f) / 180);
+
+                mLight2D.color = Color.Lerp(LightOfMidNight.color, LightOfMidDay.color, (mRevolution.Angle - 180f) / 180);
+            }
+        }
+        else
+        {
+            if (mRevolution.Angle <= 180)
+            {
+                mLight2D.pointLightOuterRadius = Mathf.Lerp(LightOfMidNight.scale, LightOfMidDay.scale, mRevolution.Angle / 180);
+
+                mLight2D.color = Color.Lerp(LightOfMidNight.color, LightOfMidDay.color, mRevolution.Angle / 180);
+            }
+            else if (mRevolution.Angle <= 360)
+            {
+                mLight2D.pointLightOuterRadius = Mathf.Lerp(LightOfMidDay.scale, LightOfMidNight.scale, (mRevolution.Angle - 180f) / 180);
+
+                mLight2D.color = Color.Lerp(LightOfMidDay.color, LightOfMidNight.color, (mRevolution.Angle - 180f) / 180);
+            }
+        }
     }
 }
