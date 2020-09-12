d5c375a405444755a71416fc96b22be1c8e03555
한대건 <han15050424@gmail.com>
2020-09-12 16:58:07 +0900

    게임 전체를 비추는 빛을 관장하는 스크립트 GlobalLightSource를 추가한다
    추가한 스크립트는 게임 전체를 비추는 빛의 변화를 지정하는 스크립트 이다.

---
+++ Assets/GlobalLightSource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class GlobalLightSource : MonoBehaviour
+{
+    [SerializeField] private Light2D mLight2D;
+
+    [SerializeField] private Color ColorOfDawn;
+    [SerializeField] private Color ColorOfNoon;
+    [SerializeField] private Color ColorOfDuty;
+    [SerializeField] private Color ColorOfMidNight;
+}
