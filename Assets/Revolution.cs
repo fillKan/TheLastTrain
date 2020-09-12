17ffcf5a1e8220b746360d2dd5bbaf5edbdfe413
한대건 <han15050424@gmail.com>
2020-09-12 13:09:46 +0900

    광원이 공전하도록 한다
    각 광원(해, 달)은 하나의 오브젝트를 중심으로 하여 그 주위를 공전하도록 한다.
    공전은 Revolution스크립트를 통해 구현되며, 속도는 인스펙터창의 Speed의 값을 설정하여 조절할 수 있다.
    
    씬에 Revolution오브젝트를 추가하고, 그 오브젝트의 하위 오브젝트로 구름을 비추는 광원과 배경을 비추는
    광원을을 추가한다.

---
+++ Assets/Revolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolution : MonoBehaviour
+{
+    private float mSpeed
+    { get => Time.deltaTime * Time.timeScale * Speed; }
+
+    [SerializeField]
+    private float Speed;
+
+    private void OnEnable()
+    {
+        StartCoroutine(EUpdate());
+    }
+    private IEnumerator EUpdate()
+    {
+        while (gameObject.activeSelf)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.back * mSpeed + transform.rotation.eulerAngles);
+
+            yield return null;
+        }
+    }
+}
