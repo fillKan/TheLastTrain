f21f418da08c9fd16b0e0c8a7b5187f3918926e6
한대건 <han15050424@gmail.com>
2020-09-12 16:51:04 +0900

    각 시간별로 색상을 전환하는 기능을 구현한다
    인스펙터 창에서 지정한 색상과 대치되는 시기에 색상이 변화하도록 한다

--- Assets/LightSource.cs
+++ Assets/LightSource.cs
@@ -34,14 +34,17 @@ public class LightSource : MonoBehaviour
             if (mRevolution.Angle <= 90)
             {
                 // Dawn -> Noon
+                mLight2D.color = Color.Lerp(ColorOfNoon, ColorOfDawn, mRevolution.Angle / 90f);
             }
             else if (mRevolution.Angle <= 360 && mRevolution.Angle > 270)
             {
                 // Noon -> Duty
+                mLight2D.color = Color.Lerp(ColorOfDuty, ColorOfNoon, (mRevolution.Angle - 270f) / 90f);
             }
             else if (mRevolution.Angle <= 270 && mRevolution.Angle > 180)
             {
                 // Duty -> Dawn
+                mLight2D.color = Color.Lerp(ColorOfDawn, ColorOfDuty, (mRevolution.Angle - 180f) / 90f);
             }
         }
         else
@@ -49,14 +52,17 @@ public class LightSource : MonoBehaviour
             if (mRevolution.Angle <= 270 && mRevolution.Angle > 180)
             {
                 // Dawn -> Noon
+                mLight2D.color = Color.Lerp(ColorOfNoon, ColorOfDawn, (mRevolution.Angle - 180f) / 90f);
             }
             else if (mRevolution.Angle <= 180 && mRevolution.Angle > 90)
             {
                 // Noon -> Duty
+                mLight2D.color = Color.Lerp(ColorOfDuty, ColorOfNoon, (mRevolution.Angle - 90f) / 90f);
             }
             else if (mRevolution.Angle <= 90)
             {
                 // Duty -> Dawn
+                mLight2D.color = Color.Lerp(ColorOfDawn, ColorOfDuty, mRevolution.Angle / 90f);
             }
         }
     }
