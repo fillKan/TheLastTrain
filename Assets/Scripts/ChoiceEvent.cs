69741da0406149a2286973093f4da2fc536d99f2
한대건 <han15050424@gmail.com>
2020-09-14 12:31:19 +0900

    선택 카드를 선택했을때의 이벤트에대한 정보를 담는 ChoiceEvent클래스를 추가한다
    인스펙터 창에서 이 선택지가 '차량 추가'인지 '정책 시행'인지를 구분하도록 하며,
    선택지에 따른 행동또한 제어할 수 있도록 한다!
    
    선택지를 선택했을 때의 동작은 ChooseThis함수를 통해 제어한다(아직 작성되지 않음)

---
+++ Assets/Scripts/ChoiceEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using InGame.UI.Resource;
+
+public enum ResourceType
+{
+    None, Population, Food, Support
+}
+
+public class ChoiceEvent : MonoBehaviour
+{
+    [SerializeField] private bool IsAdditionCompartment;
+    [SerializeField] private bool IsEnforcementPolicy;
+
+    [Header("Add Compartment")]
+    [SerializeField] private ResourceType AddResourceType;
+    [SerializeField] private int          AddResourceAmount;
+    [SerializeField] private GameObject   AddCompartment;
+
+    [Header("Policy Enforcement")]
+    [SerializeField] private UnityEvent EnforcePolicy;
+
+    public void ChooseThis()
+    {
+        // To do ...
+    }
+}
