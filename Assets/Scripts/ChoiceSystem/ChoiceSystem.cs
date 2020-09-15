f7a0e2a406cd4176f77e64f17a18f225be706327
한대건 <han15050424@gmail.com>
2020-09-15 09:04:49 +0900

    확률에따라 '차량 추가'와 '정책 시행'이벤트를 실행하도록 한다
    인스펙터 창에서 확률을 지정할 수 있으며, 설정하는 값은 '차량 추가'카드의 확률이다.
    '정책 시행'카드의 확률은 1 - 차량 추가 카드의 확률로 정해진다.
    
    이렇게 카드의 종류가 정해지면, 값을 하나 정하고 그 값과 가장 근사한 확률을 가진 카드를
    하나 고른뒤에, 활성화 시키고 인스펙터에서 설정한 CadrPositions의 값의 위치에 배치한다.
    
    지금 이 기능은 최적화의 여지가 더 있다. 배열을 정렬한 뒤에 이진탐색을 통해 더욱 효과적인
    탐색을 할 수 있기 때문이다.

--- Assets/Scripts/ChoiceSystem/ChoiceSystem.cs
+++ Assets/Scripts/ChoiceSystem/ChoiceSystem.cs
@@ -4,12 +4,52 @@ using UnityEngine;
 
 public class ChoiceSystem : MonoSingleton<ChoiceSystem>
 {
+    [SerializeField] [Range(0f, 1f)]
+    private float TrainCardProbability;
+
+    [SerializeField][Space]
+    private Vector2[] CadrPositions;
+
+    [SerializeField]
+    private ChoiceCard[] TrainCards;
+
+    [SerializeField]
+    private ChoiceCard[] PolicyCards;
+
     private void Awake()
     {
         GameEvent.Instance.DescribeMonthEvent(ShowUpChoiceCards);
     }
     private void ShowUpChoiceCards()
     {
-        // To do . . .
+        for (int i = 0; i < 3; i++)
+        {
+            if (Random.value <= TrainCardProbability)
+            {
+                EnableCard(i, TrainCards);
+            }
+            else
+            {
+                EnableCard(i, PolicyCards);
+            }
+        }
+    }
+    private void EnableCard(int index, ChoiceCard[] cards)
+    {
+        int selectIndex = 0;
+
+        float probability = Random.value;
+
+        float closestValue = 0f;
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            if (Mathf.Abs(cards[i].GetProbability - probability) < closestValue) {
+                closestValue = Mathf.Abs(cards[selectIndex = i].GetProbability - probability);
+            }
+        }
+        cards[selectIndex].transform.localPosition = CadrPositions[index];
+
+        cards[selectIndex].gameObject.SetActive(true);
     }
 }
