2977a2277e14483161341592f963464ca5a00cc3
한대건 <han15050424@gmail.com>
2020-09-15 17:22:45 +0900

    열차 카드 배열을 정렬하도록 한다
    MergeSort알고리즘을 사용하여 정렬한다. 코드는 인터넷에서 따온코드를 조금 개조해서 사용했다.
    더 보수할 점은 선택타드의 확률이 바뀔때마다 정렬을 하도록 변경하는 것이다.

--- Assets/Scripts/ChoiceSystem/ChoiceSystem.cs
+++ Assets/Scripts/ChoiceSystem/ChoiceSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ChoiceSystem : MonoSingleton<ChoiceSystem>
 {
@@ -23,16 +24,20 @@ public class ChoiceSystem : MonoSingleton<ChoiceSystem>
         GameEvent.Instance.SubscribeMonthEvent(ShowUpChoiceCards);
 
         mStartByWeekTable = GameEvent.Instance.GetWeek.GetWeekTable;
+
+        ChoiceCard[] tempArray = new ChoiceCard[TrainCards.Length];
+
+        MergeSort(TrainCards, 0, TrainCards.Length - 1, tempArray, (A, B) => A.GetProbabilities[0] < B.GetProbabilities[0]);
     }
     private void ShowUpChoiceCards()
     {
         for (int i = 0; i < 3; i++)
         {
-            if (Random.value <= TrainCardProbability)
+            if (UnityEngine.Random.value <= TrainCardProbability && TrainCards.Length > 0)
             {
                 EnableCard(i, TrainCards);
             }
-            else
+            else if (PolicyCards.Length > 0)
             {
                 EnableCard(i, PolicyCards);
             }
@@ -42,7 +47,7 @@ public class ChoiceSystem : MonoSingleton<ChoiceSystem>
     {
         int selectIndex = 0;
 
-        float probability = Random.value;
+        float probability = UnityEngine.Random.value;
 
         float closestValue = float.MaxValue;
 
@@ -67,5 +72,48 @@ public class ChoiceSystem : MonoSingleton<ChoiceSystem>
         cards[selectIndex].transform.localPosition = CadrPositions[index];
 
         cards[selectIndex].gameObject.SetActive(true);
+    }
+
+    void MergeSort<T>(T []sortingArray, int lowIndex, int highIndex, T []tempArray, Func<T,T,bool> isRValueBigger)
+    {
+        // 1. base condition
+        if (lowIndex >= highIndex) return;
+
+        // 2. divide
+        int mid = (lowIndex + highIndex) / 2;
+
+        // 3. conquer
+        MergeSort(sortingArray, lowIndex,       mid, tempArray, isRValueBigger);
+        MergeSort(sortingArray, mid + 1 , highIndex, tempArray, isRValueBigger);
+
+        // 4. combine
+        int i = lowIndex;
+        int j = mid + 1;
+        int k = lowIndex;
+
+        for (; k <= highIndex; ++k)
+        {
+            if (j > highIndex)
+            {
+                tempArray[k] = sortingArray[i++];
+            }
+            else if (i > mid)
+            {
+                tempArray[k] = sortingArray[j++];
+            }
+            else if (isRValueBigger(sortingArray[i], sortingArray[j]))
+            {
+                tempArray[k] = sortingArray[i++];
+            }
+            else
+            {
+                tempArray[k] = sortingArray[j++];
+            }
+        }
+        // 5. copy
+        for (i = lowIndex; i <= highIndex; ++i)
+        {
+            sortingArray[i] = tempArray[i];
+        }
     }
 }
