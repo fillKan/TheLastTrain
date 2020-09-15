e720d3ca43768dbb4c77bcaed2cfd94d5328b393
한대건 <han15050424@gmail.com>
2020-09-15 22:06:28 +0900

    각 정책 카드들의 정책을 클래스로 구분한다
    PolicyData(확정된 이름이 아닙니다 앓~~)가 정책들의 허브 역할을 하도록 하고,
    클래스로 구분된 정책들의 함수를 실행시키는 것으로 간단하게 동작을 제어할 수 있도록 한다.
    
    이러한 동작들은 인터페이스로 구현된다.

---
+++ Assets/Scripts/ChoiceSystem/PolicyEvents/PolicyData.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System;
+
+public class PolicyData : MonoSingleton<PolicyData>
+{
+
+}
+public interface IEnforcementable
+{
+    Policy GetPolicy
+    {
+        get;
+    }
+    void Enforce();
+}
+
+public class MissionaryWork : IEnforcementable
+{
+    public Policy GetPolicy
+    {
+        get => Policy.MissionaryWork;
+    }
+    public void Enforce()
+    {
+        GameEvent.Instance.GetResource.ApplyLeaderShip(3);
+    }
+}
+
+public class FoodSaving : IEnforcementable
+{
+    public Policy GetPolicy
+    {
+        get => Policy.FoodSaving;
+    }
+    public void Enforce()
+    {
+        int amount = (int)(GameEvent.Instance.InitResourceTable.foodTable.Max * 0.3f);
+
+        GameEvent.Instance.GetResource.ApplyFood(amount);
+
+        GameEvent.Instance.GetResource.ApplyLeaderShip(-4);
+    }
+}
+public class PopulationDownSize : IEnforcementable
+{
+    public Policy GetPolicy
+    {
+        get => Policy.PopulationDownSize;
+    }
+    public void Enforce()
+    {
+        int amount = (int)(GameEvent.Instance.InitResourceTable.populationTable.Max * 0.4f);
+
+        GameEvent.Instance.GetResource.ApplyPopulation(-amount);
+
+        GameEvent.Instance.GetResource.ApplyLeaderShip(-5);
+    }
+}
+
+public class MedicalIndustry : IEnforcementable
+{
+    public Policy GetPolicy
+    {
+        get => Policy.MedicalIndustry;
+    }
+    public void Enforce()
+    {
+        int amount = (int)(GameEvent.Instance.InitResourceTable.foodTable.Max * 0.3f);
+
+        GameEvent.Instance.GetResource.ApplyPopulation(-amount);
+
+        GameEvent.Instance.GetResource.ApplyLeaderShip(5);
+    }
+}
+public class ExtraWork : IEnforcementable
+{
+    public Policy GetPolicy
+    {
+        get => Policy.ExtraWork;
+    }
+    public void Enforce()
+    {
+        int population = (int)(GameEvent.Instance.InitResourceTable.populationTable.Max * 0.1f);
+
+        GameEvent.Instance.GetResource.ApplyPopulation(-population * 2);
+
+        GameEvent.Instance.GetResource.ApplyFood(population * 3);
+    }
+}
