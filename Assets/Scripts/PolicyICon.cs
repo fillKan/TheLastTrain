2423a2fc5ed9ec75512875967774f632f7ebcb53
한대건 <han15050424@gmail.com>
2020-09-17 07:59:48 +0900

    z정책아이콘만의 스크립트를 추가한다
    PolicyIConControl스크립트는 추가한 정책 아이콘의 스크립트를 저장하도록 변경한다.

---
+++ Assets/Scripts/PolicyICon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PolicyICon : MonoBehaviour
+{
+    public Animator GetAnimator;
+    public Image GetImage;
+
+    private void Reset()
+    {
+        Debug.Assert(TryGetComponent(out GetImage));
+        Debug.Assert(TryGetComponent(out GetAnimator));
+    }
+}
--- Assets/Scripts/PolicyIConControl.cs
+++ Assets/Scripts/PolicyIConControl.cs
@@ -5,41 +5,17 @@ using UnityEngine.UI;
 
 public class PolicyIConControl : MonoBehaviour
 {
-    private const int ICON_COUNT = 2;
+    private const int ICON_COUNT = 3;
 
-    public class Preset
-    {
-        public Animator Animator;
-        public Image Image;
-
-        public Preset(Animator animator, Image image)
-        {
-            Animator = animator; Image = image;
-        }
-    }
-    [SerializeField] private Animator[] PolicyAnimator = new Animator[ICON_COUNT];
-    [SerializeField] private    Image[]    PolicyImage = new    Image[ICON_COUNT];
-
-    private List<Preset> mLPolicy = new List<Preset>(ICON_COUNT);
-
+    [SerializeField] private PolicyICon[] policyICons = new PolicyICon[ICON_COUNT];
 
     private void Reset()
     {
         for (int i = 0; i < ICON_COUNT; i++)
         {
-            if (transform.GetChild(i).TryGetComponent(out Image image)) {
-                PolicyImage[i] = image;
+            if (transform.GetChild(i).TryGetComponent(out PolicyICon policyICon)) {
+                policyICons[i] = policyICon;
             }
-            if (transform.GetChild(i).TryGetComponent(out Animator animator)) {
-                PolicyAnimator[i] = animator;
-            }
-        }
-    }
-    private void OnEnable()
-    {
-        for (int i = 0; i < ICON_COUNT; i++)
-        {
-            mLPolicy.Add(new Preset(PolicyAnimator[i], PolicyImage[i]));
         }
     }
     public void AddPolicy(Policy policy)
