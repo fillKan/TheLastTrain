9340cc20e3a6f960aca049f7e7622e6b3fb9239f
한대건 <han15050424@gmail.com>
2020-09-16 20:39:44 +0900

    애니메이터와 이미지의 정보를 담는 클래스 Preset을 추가한다
    Preset리스트를 통해 정책 아이콘을 관리하도록 한다.

--- Assets/Scripts/PolicyIConControl.cs
+++ Assets/Scripts/PolicyIConControl.cs
@@ -5,24 +5,43 @@ using UnityEngine.UI;
 
 public class PolicyIConControl : MonoBehaviour
 {
-    private const int ICON_COUNT = 2; 
+    private const int ICON_COUNT = 2;
 
-    [SerializeField] private Animator[] mPoliyAnimator = new Animator[ICON_COUNT];
-    [SerializeField] private    Image[]    mPoliyImage = new    Image[ICON_COUNT];
+    public class Preset
+    {
+        public Animator Animator;
+        public Image Image;
+
+        public Preset(Animator animator, Image image)
+        {
+            Animator = animator; Image = image;
+        }
+    }
+    [SerializeField] private Animator[] PolicyAnimator = new Animator[ICON_COUNT];
+    [SerializeField] private    Image[]    PolicyImage = new    Image[ICON_COUNT];
+
+    private List<Preset> mLPolicy = new List<Preset>(ICON_COUNT);
+
 
     private void Reset()
     {
         for (int i = 0; i < ICON_COUNT; i++)
         {
             if (transform.GetChild(i).TryGetComponent(out Image image)) {
-                mPoliyImage[i] = image;
+                PolicyImage[i] = image;
             }
             if (transform.GetChild(i).TryGetComponent(out Animator animator)) {
-                mPoliyAnimator[i] = animator;
+                PolicyAnimator[i] = animator;
             }
         }
     }
-
+    private void OnEnable()
+    {
+        for (int i = 0; i < ICON_COUNT; i++)
+        {
+            mLPolicy.Add(new Preset(PolicyAnimator[i], PolicyImage[i]));
+        }
+    }
     public void AddPolicy(Policy policy)
     {
         // To do ...
