ba9e58f4fb026be83a039fec7b3a92a2a29d000d
한대건 <han15050424@gmail.com>
2020-09-16 19:13:19 +0900

    정책 아이콘을 제어하는 스크립트를 작성한다
    Reset함수를 통해 미리 자식 오브젝트들의 정보를 캐싱해두도록 한다.
    그리고 정책을 추가하는 AddPolicy함수를 통해 정책을 추가할 수 있도록 틀을..짜놓았다.

--- Assets/Scripts/PolicyIConControl.cs
+++ Assets/Scripts/PolicyIConControl.cs
@@ -1,8 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PolicyIConControl : MonoBehaviour
 {
-    
+    private const int ICON_COUNT = 2; 
+
+    [SerializeField] private Animator[] mPoliyAnimator = new Animator[ICON_COUNT];
+    [SerializeField] private    Image[]    mPoliyImage = new    Image[ICON_COUNT];
+
+    private void Reset()
+    {
+        for (int i = 0; i < ICON_COUNT; i++)
+        {
+            if (transform.GetChild(i).TryGetComponent(out Image image)) {
+                mPoliyImage[i] = image;
+            }
+            if (transform.GetChild(i).TryGetComponent(out Animator animator)) {
+                mPoliyAnimator[i] = animator;
+            }
+        }
+    }
+
+    public void AddPolicy(Policy policy)
+    {
+        // To do ...
+    }
 }
