a64121e3f558dbd244e02b4483ff0e5ab3ff3911
한대건 <han15050424@gmail.com>
2020-09-17 09:39:22 +0900

    정책 아이콘이 상황에 따라 적절한 애니메이션을 취하도록 한다
    이는 PolicyIConControl스크립트를 통해 구현한다. 현재는 테스트를 위해 아무 키나 누르면 작동하도록 했다.
    이 기능을 하드코딩으로 만들어서 영 아니긴 하지만..그래도! 구현했다는 거에 의미를 두자고요 앓~~~~~~~

--- Assets/Scripts/PolicyIConControl.cs
+++ Assets/Scripts/PolicyIConControl.cs
@@ -9,6 +9,10 @@ public class PolicyIConControl : MonoBehaviour
 
     [SerializeField] private PolicyICon[] policyICons = new PolicyICon[ICON_COUNT];
 
+    private int mEmptyIndex = 0;
+    private int mRightIndex = 2;
+    private int  mLeftIndex = 1;
+
     private void Reset()
     {
         for (int i = 0; i < ICON_COUNT; i++)
@@ -18,8 +22,23 @@ public class PolicyIConControl : MonoBehaviour
             }
         }
     }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            AddPolicy((Policy)Random.Range(1, 6));
+        }
+    }
     public void AddPolicy(Policy policy)
     {
-        // To do ...
+        policyICons[mRightIndex].SetAniStste(PolicyAniState.LeftMove);
+        policyICons[mEmptyIndex].SetAniStste(PolicyAniState.OnPolicy);
+        policyICons[ mLeftIndex].SetAniStste(PolicyAniState.DisPolicy);
+
+        policyICons[mEmptyIndex].GetImage.sprite = PolicyHub.Instance.GetPolicyICon(policy);
+
+        mRightIndex = (mRightIndex + 1) > 2 ? 0 : (mRightIndex + 1);
+        mEmptyIndex = (mEmptyIndex + 1) > 2 ? 0 : (mEmptyIndex + 1);
+         mLeftIndex = ( mLeftIndex + 1) > 2 ? 0 : ( mLeftIndex + 1);
     }
 }
