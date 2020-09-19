217e7e133228367d3da53449137fbbf9891a43fb
ParkHyeonJae <md101106@gmail.com>
2020-09-19 17:44:32 +0900

    인 게임 사운드 추가 및 매니저 수정

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -3,92 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace InGame.Sound
+[System.Serializable]
+public class Audio
 {
-    [System.Serializable]
-    public class Audio
+    public string name;
+    public AudioClip sound;
+}
+public enum SoundType
+{
+    BGM,
+    FX,
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    [SerializeField] private List<Audio> audioList;
+
+    private Dictionary<string, AudioClip> audios = new Dictionary<string, AudioClip>();
+
+    public Dictionary<string, AudioClip> GetAudio() => audios;
+
+    private AudioSource BGMSource;
+    private AudioSource FXSource;
+
+    public float BGMVolume;
+    public float FXVolume;
+
+
+    private void Start()
     {
-        public string name;
-        public AudioClip sound;
-    }
-    public enum SoundType
-    {
-        BGM,
-        FX,
-    }
-
-    [RequireComponent(typeof(AudioSource))]
-    public class SoundManager : MonoBehaviour
-    {
-        [SerializeField] private List<Audio> audioList;
-
-        private Dictionary<string, AudioClip> audios = new Dictionary<string, AudioClip>();
-
-        public Dictionary<string, AudioClip> GetAudio() => audios;
-
-        private AudioSource BGMSource;
-        private AudioSource FXSource;
-
-        public float BGMVolume;
-        public float FXVolume;
-
-
-        private void Awake()
+        if (audioList.Count > 0)
         {
-            if (audioList.Count > 0)
+            foreach (var v in audioList)
             {
-                foreach (var v in audioList)
-                {
-                    audios.Add(v.name, v.sound);
-                }
-            }
-            BGMSource = GetComponent<AudioSource>();
-            FXSource = GetComponent<AudioSource>();
-        }
-
-        private void Start()
-        {
-            StartSound();
-        }
-
-        void StartSound()
-        {
-            SetVolume(1.0f, SoundType.BGM);
-            SetVolume(1.0f, SoundType.FX);
-
-            PlayLoopSound("BGM");
-        }
-
-
-        public void SetVolume(float volume, SoundType type)
-        {
-            switch (type)
-            {
-                case SoundType.BGM:
-                    BGMVolume = volume;
-                    break;
-                case SoundType.FX:
-                    FXVolume = volume;
-                    break;
-                default:
-                    break;
+                audios.Add(v.name, v.sound);
             }
         }
+        BGMSource = GetComponent<AudioSource>();
+        FXSource = GetComponent<AudioSource>();
 
-        public void PlayOneShot(string name)
-        {
-            FXSource.volume = FXVolume;
-            FXSource.PlayOneShot(GetAudio()[name]);
-        }
+        StartSound();
+    }
 
-        public void PlayLoopSound(string name)
+    void StartSound()
+    {
+        SetVolume(1.0f, SoundType.BGM);
+        SetVolume(1.0f, SoundType.FX);
+
+        PlayLoopSound("BGM");
+    }
+
+
+    public void SetVolume(float volume, SoundType type)
+    {
+        switch (type)
         {
-            if (!GetAudio().ContainsKey(name))
-                return;
-            BGMSource.volume = BGMVolume;
-            BGMSource.clip = GetAudio()[name];
-            BGMSource.loop = true;
-            BGMSource.Play();
+            case SoundType.BGM:
+                BGMVolume = volume;
+                break;
+            case SoundType.FX:
+                FXVolume = volume;
+                break;
+            default:
+                break;
         }
+    }
+
+    public void PlayOneShot(string name)
+    {
+        FXSource.volume = FXVolume;
+        FXSource.PlayOneShot(GetAudio()[name]);
+    }
+
+    public void PlayLoopSound(string name)
+    {
+        if (!GetAudio().ContainsKey(name))
+            return;
+        BGMSource.volume = BGMVolume;
+        BGMSource.clip = GetAudio()[name];
+        BGMSource.loop = true;
+        BGMSource.Play();
     }
 }
