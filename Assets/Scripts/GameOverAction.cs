3c505f47ef4a7032565d34dad3e2706043939e20
한대건 <han15050424@gmail.com>
2020-10-03 08:26:35 +0900

    싱글톤 객체일 필요가없는 GameObserver를 리팩토링한다
    우선 이름을 GameObserver에서 GameOverAction으로 변경했다.
    
    변수의 이름을 '이 변수가 어떤 값을 저장하는지'를 알아볼 수 있도록 변경하고,
    public으로 사용하는 변수를 private로 변경했다. 이 중에서 인스펙터창에서 값을 설정해주어야 하는
    변수는 씨리얼라이즈했다.
    
    그리고 매번 싱글톤의 값을 불러오는 방식 대신 클래스를 캐싱하여 사용하도록 변경하였고 이 외에도
    몇가지의 최적화를 하였다.

---
+++ Assets/Scripts/GameOverAction.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using InGame.UI.Week;
+
+public class GameOverAction : MonoBehaviour
+{
+    [SerializeField]
+    private int mEndingIndex;
+
+    private int mSurvivalDay;
+
+    private Week mGameDateTime;
+
+    private ResourceTable mResourceTable;
+
+    [SerializeField]
+    private GameObject[] Themas;
+
+    private void Awake()
+    {
+        Themas[Random.Range(0, Themas.Length)].SetActive(true);
+    }
+
+    private void Start()
+    {
+        GameEvent.Instance.SubscribeBubbleEvent(() => mSurvivalDay++);
+
+        mGameDateTime =
+            GameEvent.Instance.GetWeek;
+
+        mResourceTable = 
+            GameEvent.Instance.GetResource.GetResourceTable;
+    }
+    private void Update()
+    {
+        if (IsGameOver())
+        {
+            GameObject day = new GameObject("DaySaver", typeof(DaySaver));
+
+            if (day.TryGetComponent(out DaySaver daySaver))
+            {
+                daySaver.DaySave(mSurvivalDay, mGameDateTime.GetWeekTable);
+
+                DontDestroyOnLoad(day);
+            }
+            SceneManager.LoadScene(mEndingIndex);
+        }
+    }
+    private bool IsGameOver()
+    {
+        return mResourceTable.foodTable.Now       <= 0 ||
+               mResourceTable.leaderShipTable.Now <= 0 ||
+               mResourceTable.populationTable.Now <= 0;
+    }
+}
