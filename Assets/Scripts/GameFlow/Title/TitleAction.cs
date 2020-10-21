d2d771a8674c189f8d7cb292d4055c9d18788b60
한대건 <han15050424@gmail.com>
2020-10-21 14:27:30 +0900

    타이틀에서 활성화된 배경이 이어지도록 TitleAction스크립트를 수정한다
    게임이 시작될 때 TitleAction스크립트는 인스펙터에서 ThemaObjets에 지정한 오브젝트들 중 하나를 활성화한다.
    이렇게 활성화된 테마와 TitleActon는 DontDestroyOnLoad되어(테마들이 TitleAction의 자식으로 귀속된다)
    인게임씬에서도 테마로 사용된다.
    
    아직 GameOverAction의 테마를 활성화하는 코드를 수정하지 못했기 때문에..오류가 있다.

--- Assets/Scripts/GameFlow/Title/TitleAction.cs
+++ Assets/Scripts/GameFlow/Title/TitleAction.cs
@@ -7,5 +7,17 @@ public class TitleAction : MonoBehaviour
 {
     [SerializeField]
     private int MainGameScenceIndex;
-    public void MainGameLoad() => SceneManager.LoadScene(MainGameScenceIndex);
+
+    [Space()][SerializeField]
+    private GameObject[] ThemaObjets;
+    
+    public void MainGameLoad() {
+        SceneManager.LoadScene(MainGameScenceIndex);
+    }
+
+    private void Awake()
+    {
+        ThemaObjets[Random.Range(0, ThemaObjets.Length)].SetActive(true);
+        DontDestroyOnLoad(gameObject);
+    }
 }
