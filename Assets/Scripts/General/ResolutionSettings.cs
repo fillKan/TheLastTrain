47b6a63eaf452d60ad01d6c1f089ae6e79f98cd1
박현재 박 <md101106@gmail.com>
2021-01-15 14:10:52 +0900

    Add Resoulution Script
    Fix Resolution When Build and Play form Mobile

---
+++ Assets/Scripts/General/ResolutionSettings.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSettings : MonoBehaviour
+{
+    private static Camera _camera = null;
+    public Camera GetCamera {
+        get => (_camera) ? _camera : _camera = Camera.main;
+    }
+    private Vector2 Resolution = new Vector2(18, 9);
+    private void Awake()
+    {
+        if (Application.isEditor) return;
+        Rect rect = GetCamera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)Resolution.x / Resolution.y);
+        float scalewidth = 1f / scaleheight;
+
+        if (scaleheight < 1)
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        GetCamera.rect = rect;
+
+        GetCamera.backgroundColor = Color.black;
+    }
+    void OnPreCull()
+    {
+        if (Application.isEditor) return;
+        Rect wp = Camera.main.rect;
+        Rect nr = new Rect(0, 0, 1, 1);
+
+        Camera.main.rect = nr;
+        GL.Clear(true, true, Color.black);
+
+        Camera.main.rect = wp;
+
+    }
+
+}
