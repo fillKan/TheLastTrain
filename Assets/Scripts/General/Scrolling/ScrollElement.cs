0c4eb12f0c5568ccea46e4703bc8d93925187160
kimbokChi <han15050424@gmail.com>
2021-01-27 16:42:06 +0900

    Scroller를 구성하는 ScrollElement스크립트를 추가한다
    ScrollElement는 자신이 보이지 않게 되었을때 실행하는 이벤트를 제공한다.
    
    또한 자신의 머리와 꼬리의 위치를 제공한다. 머리는 피벗에서 이미지의 왼쪽 끝부분까지의 거리이고,
    꼬리는 피벗에서 이미지의 오른족 끝부분까지의 거리를 말한다.

---
+++ Assets/Scripts/General/Scrolling/ScrollElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollElement : MonoBehaviour
+{
+    public event Action<ScrollElement> InvisibleEvent;
+
+    public Vector2 Head
+    {
+        get
+        {
+            Vector2 value = transform.position;
+            return  value + Vector2.left * _PivotToHeadLength;
+        }
+    }
+    public Vector2 Tail
+    {
+        get
+        {
+            Vector2 value = transform.position;
+            return  value + Vector2.right * _PivotToTailLength;
+        }
+    }
+
+    [SerializeField] private float _PivotToHeadLength;
+    [SerializeField] private float _PivotToTailLength;
+
+    private void OnBecameInvisible()
+    {
+        InvisibleEvent?.Invoke(this);
+    }
+}
